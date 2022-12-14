d724a220794003d591c32d42b797c3f93bd06fe9
MindulaHewadewage <mindufernando@gmail.com>
2022-12-14 16:00:40 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// elementi nella lista
+const listItems = ['lattuga', 'uova', 'pane', 'latte'];
+
+
+// elemento del dom
+const targetElement = document.getElementById('target');
+
+// while
+let i = 0;
+while(i < listItems.length){
+    i++;
+}
+
+console.log(listItems);
