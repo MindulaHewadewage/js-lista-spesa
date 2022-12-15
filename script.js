d66eb494835efa507a8debe6e99e943c1ea0e62c
MindulaHewadewage <mindufernando@gmail.com>
2022-12-15 15:06:59 +0100

    correzzione esercizio

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 // elementi nella lista
 const listItems = ['lattuga', 'uova', 'pane', 'latte'];
+const listElement = document.getElementById('shopping--list')
 
 
-// elemento del dom
-const targetElement = document.getElementById('target');
 
 // while
 let i = 0;
 while(i < listItems.length){
+
+    const item = document.createElement('li');
+    item.append(shoppingList[i]);
+    listElement.appendChild (item);
     i++;
 }
 
